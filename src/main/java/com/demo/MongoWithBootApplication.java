331727602e22cf98d9460913a84c0b7ff4eec0c0
ATOM\Mehraj Malik <malikmehraj16@gmail.com>
2018-01-02 22:23:31 +0530

    reading env properties

--- src/main/java/com/demo/MongoWithBootApplication.java
+++ src/main/java/com/demo/MongoWithBootApplication.java
@@ -1,14 +1,25 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 // @EnableScheduling
+// @PropertySource("file:config.properties")
+// @PropertySource("#{systemEnvironment['LOCATION']}")
 public class MongoWithBootApplication {
 
+	@Value("#{systemEnvironment['LOCATION']}")
+	private String location;
+
+	public void hi() {
+		System.out.println("--- ENV "+location);
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(MongoWithBootApplication.class, args);
+		SpringApplication.run(MongoWithBootApplication.class, args).getBean(MongoWithBootApplication.class).hi();;
+		
 
 	}
 }
