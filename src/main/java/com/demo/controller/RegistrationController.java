5a34b89b1d9da822ee446bdc34d53189a295522e
Iti Gupta <iti.gupta@kelltontech.com>
2018-01-17 15:33:45 +0530

    HT-999 #comment API To get all the users #time 15m

--- src/main/java/com/demo/controller/RegistrationController.java
+++ src/main/java/com/demo/controller/RegistrationController.java
@@ -1,19 +1,31 @@
 package com.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.UserRegistrationVM;
+import com.demo.mv.HotelMV;
+import com.demo.mv.UserRegistrationMV;
 import com.demo.service.UserRegistrationService;
+import com.demo.successcode.HotelSuccessCode;
 import com.demo.successcode.UserRegistrationSuccessCode;
+import com.demo.util.Constants;
 import com.demo.util.CustomResponse;
+import com.demo.util.PageableInfo;
 
 /***
  * 
@@ -25,20 +37,45 @@ import com.demo.util.CustomResponse;
  */
 @RestController
 @RequestMapping("/users")
-//@CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
+// @CrossOrigin(origins="http://localhost:4200", allowedHeaders="*")
 public class RegistrationController {
 
+	private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
+
 	private UserRegistrationService userRegistrationService;
-	
+
 	public RegistrationController(UserRegistrationService userRegistrationService) {
-		this.userRegistrationService=userRegistrationService;
+		this.userRegistrationService = userRegistrationService;
 	}
 
+	/***
+	 * @author Iti Gupta
+	 * @param userRegistrationVM
+	 * @return Custom Response
+	 */
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<CustomResponse> userRegistration(@RequestBody @Valid UserRegistrationVM userRegistrationVM) {
 
 		return new ResponseEntity<>(new CustomResponse(UserRegistrationSuccessCode.USER_CREATED.getCode(),
-				UserRegistrationSuccessCode.USER_CREATED.getMessage(), userRegistrationService.userRegistration(userRegistrationVM), null), HttpStatus.OK);
+				UserRegistrationSuccessCode.USER_CREATED.getMessage(),
+				userRegistrationService.userRegistration(userRegistrationVM), null), HttpStatus.OK);
 
+	}
+
+	/***
+	 * @author Iti Gupta
+	 * @param pageable
+	 * @return Custom Response
+	 */
+	@GetMapping
+	public ResponseEntity<CustomResponse> getAllUsers(Pageable pageable) {
+		log.info("Enter in getAllUsers()...");
+		PageableInfo<UserRegistrationMV> users = userRegistrationService.getAll(pageable);
+		Map<String, Object> pageinfo = new HashMap<>(1);
+		pageinfo.put(Constants.PAGE_INFO, users.getPageInfo());
+		return new ResponseEntity<>(
+				new CustomResponse(UserRegistrationSuccessCode.USER_FETCHED.getCode(),
+						UserRegistrationSuccessCode.USER_FETCHED.getMessage(), users.getContent(), pageinfo),
+				HttpStatus.OK);
 	}
 }
--- src/main/java/com/demo/service/impl/UserRegistrationServiceImpl.java
+++ src/main/java/com/demo/service/impl/UserRegistrationServiceImpl.java
@@ -3,14 +3,19 @@ package com.demo.service.impl;
 import static com.demo.error.UserRegistrationExceptionSupplier.EMAIL_NOT_UNIQUE;
 import static com.demo.error.UserRegistrationExceptionSupplier.PHONE_NUMBER_NOT_UNIQUE;
 import static com.demo.error.UserRegistrationExceptionSupplier.USERNAME_NOT_UNIQUE;
+import static com.demo.error.UserRegistrationExceptionSupplier.USER_FETCHED_EXCEPTION;
 import static com.demo.error.UserRegistrationExceptionSupplier.USER_REGISTRATION_EXCEPTION;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.demo.entity.UserRegistration;
@@ -19,6 +24,8 @@ import com.demo.model.UserRegistrationVM;
 import com.demo.mv.UserRegistrationMV;
 import com.demo.repository.UserRegistrationRepository;
 import com.demo.service.UserRegistrationService;
+import com.demo.util.PageInfo;
+import com.demo.util.PageableInfo;
 
 /***
  * Implementation of {@code RegistrationService}
@@ -92,11 +99,34 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
 				throw EMAIL_NOT_UNIQUE.get();
 			}
 		} catch (CustomParameterizedException e) {
-			logger.error("--- CustomParameterizedException {} ",e);
+			logger.error("--- CustomParameterizedException {} ", e);
 			throw e;
 		} catch (Exception e) {
-			logger.error("--- Exception occured while registering user {} ",e);
+			logger.error("--- Exception occured while registering user {} ", e);
 			throw USER_REGISTRATION_EXCEPTION.get();
+		}
+	}
+
+	/***
+	 * To get all the users
+	 * 
+	 * @author Iti Gupta
+	 * 
+	 */
+	@Override
+	public PageableInfo<UserRegistrationMV> getAll(Pageable pageable) {
+		try {
+			Page<UserRegistration> user = userRegistrationRepository.findAll(pageable);
+			PageableInfo<UserRegistrationMV> pageableInfo = new PageableInfo<>();
+			if (!user.getContent().isEmpty()) {
+				pageableInfo.setContent(Arrays.asList(modelMapper.map(user.getContent(), UserRegistrationMV[].class)));
+			} else {
+				pageableInfo.setContent(Collections.emptyList());
+			}
+			pageableInfo.setPageInfo(modelMapper.map(user, PageInfo.class));
+			return pageableInfo;
+		} catch (Exception e) {
+			throw USER_FETCHED_EXCEPTION.get();
 		}
 	}
 
