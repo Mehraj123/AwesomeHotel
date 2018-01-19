91d59238aa1164f3f672624c1cb636f4aa32e0aa
arun.verma <arun.verma>
2018-01-19 15:47:38 +0530

    Added Angular 4 support

--- src/main/java/com/demo/config/HazelCastCacheConfig.java
+++ src/main/java/com/demo/config/HazelCastCacheConfig.java
@@ -1,43 +1,46 @@
-package com.demo.config;
+/*package com.demo.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
-import com.hazelcast.config.Config;
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;;
 
 @Configuration
 @EnableCaching
 public class HazelCastCacheConfig {
 
+	private static final Logger logger = LoggerFactory.getLogger(HazelCastCacheConfig.class);
+
+	@Value("${cache.hazelcast.host}")
+	private String host;
+
+	@Value("${cache.hazelcast.port}")
+	private String port;
+
 	@Value("${cache.hazelcast.expirationInSeconds}")
 	private int expirationInSeconds;
-	
+
+
 	@Bean
-    public Config hazelCastConfig(){
-        return new Config()
-                .setInstanceName("hazelcast-instance")
-                .addMapConfig(
-                        new MapConfig()
-                                .setName("Hotel")
-                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                                .setEvictionPolicy(EvictionPolicy.LRU)
-                                .setTimeToLiveSeconds(expirationInSeconds));
-    }
-	
-	public void sum() {
+	public HazelcastInstance hazelcastInstance() {
+		String uri = host + ":" + port;
+		logger.info("--- Connecting hazelcast client to {} ", uri);
+		Config config = new Config();
+		NetworkConfig network =  config.getNetworkConfig();
+		network.getJoin().getTcpIpConfig().setEnabled(true);
 		ClientConfig clientConfig = new ClientConfig();
-		ClientNetworkConfig  networkConfig = clientConfig.getNetworkConfig();
-		networkConfig.addAddress("");
-		//this.ClientNetworkConfig.setAddresses(new ArrayList<>());
-		
-		HazelcastClient.newHazelcastClient();
+		ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+		networkConfig.addAddress(host);
+		clientConfig.setNetworkConfig(networkConfig);
+		HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+		logger.info("--- Connected successfully to {} ", uri);
+		HazelcastInstance client = Hazelcast.newHazelcastInstance();
+		return client;
 	}
 }
+*/
